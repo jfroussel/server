4bc7830eee20f2b2d0bf6a7b851b930ff1754138
jeff <jeff@jeffHome.lan>
2018-11-08 11:48:25 +0100

    wip

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 mongoose.connect(
     //'mongodb://jeff:JFRtnjsjade2010@ds147213.mlab.com:47213/reactsound', // Mlab mongo server
-    //'mongodb://37.187.111.111:27017', // distant linux mongo server
-    'mongodb://localhost/server', //local linux mongo server
+    'mongodb://http://51.15.190.152:27017', // distant linux mongo server
+    //'mongodb://localhost:27017/server', //local linux mongo server
     { useCreateIndex: true, useNewUrlParser: true }
 )
 
